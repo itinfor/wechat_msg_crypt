c286cc50d818a4e3f02dd8dc2c22eb932e56f243
paopao7 <980569038@qq.com>
2023-08-23 23:23:45 +0800

    初始化

---
+++ src/sha1.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Itinfor;
+
+use think\Exception;
+
+include_once "errorCode.php";
+
+/**
+ * SHA1 class
+ * 计算公众平台的消息签名接口.
+ */
+class sha1
+{
+
+    /**
+     * 用SHA1算法生成安全签名
+     *
+     * @param string $token     票据
+     * @param string $timestamp 时间戳
+     * @param string $nonce     随机字符串
+     * @param        $encrypt_msg
+     *
+     * @return array
+     */
+    public function getSHA1(string $token, string $timestamp, string $nonce, $encrypt_msg)
+    {
+        //排序
+        try
+        {
+            $array = array($encrypt_msg, $token, $timestamp, $nonce);
+            sort($array, SORT_STRING);
+            $str = implode($array);
+
+            return array(ErrorCode::$OK, sha1($str));
+        } catch (Exception $e)
+        {
+            //print $e . "\n";
+            return array(ErrorCode::$ComputeSignatureError, NULL);
+        }
+    }
+
+}
